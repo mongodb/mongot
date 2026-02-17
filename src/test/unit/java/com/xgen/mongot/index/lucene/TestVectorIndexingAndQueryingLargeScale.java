6696c14fb61531636399e112200e39966c60517f
Mihir Patil <mihirmpatil@users.noreply.github.com>
2026-02-17 01:15:09 -0800

    CLOUDP-297354 - Merge large vector test cases into single class (#5281)
    GitOrigin-RevId: 4c4679f382efa6a12df68abd6c85260d518403d6

--- src/test/unit/java/com/xgen/mongot/index/lucene/TestVectorIndexingAndQueryingLargeScale.java
+++ src/test/unit/java/com/xgen/mongot/index/lucene/TestVectorIndexingAndQueryingLargeScale.java
@@ -2,16 +2,20 @@ package com.xgen.mongot.index.lucene;
 
 import static com.xgen.mongot.index.lucene.VectorIndexingAndQueryingTestHarness.buildVectorIndexDefinition;
 
+import com.googlecode.junittoolbox.ParallelParameterized;
 import com.xgen.mongot.index.definition.VectorQuantization;
 import com.xgen.mongot.index.definition.VectorSimilarity;
 import com.xgen.mongot.util.bson.Vector;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.bson.BsonDocument;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-// These tests with 10k vectors can take significant time to run and can also timeout.
-// To avoid this, the tests are split into three different subclasses of this class so that
-// each test class can be executed independently by bazel.
+// Keep the parameters explicit so the test matrix is visible and easy to tweak.
+@RunWith(ParallelParameterized.class)
 public class TestVectorIndexingAndQueryingLargeScale {
 
   static final int NUM_VECTORS = 10000;
@@ -23,47 +27,100 @@ public class TestVectorIndexingAndQueryingLargeScale {
   final VectorSimilarity vectorSimilarity;
   final boolean exact;
   final int numPartitions;
+  final Vector.VectorType vectorType;
+  final VectorQuantization vectorQuantization;
   final VectorIndexingAndQueryingTestHarness testHarness;
   List<BsonDocument> docs;
   List<? extends Vector> queryVectors;
 
-  public TestVectorIndexingAndQueryingLargeScale(
-      VectorSimilarity vectorSimilarity, boolean exact, int numPartitions) {
-    this.vectorSimilarity = vectorSimilarity;
-    this.exact = exact;
-    this.numPartitions = numPartitions;
+  public TestVectorIndexingAndQueryingLargeScale(TestCase testCase) {
+    this.vectorSimilarity = testCase.similarity();
+    this.exact = testCase.exact();
+    this.numPartitions = testCase.numPartitions();
+    this.vectorType = testCase.vectorType();
+    this.vectorQuantization = testCase.vectorQuantization();
     this.testHarness = new VectorIndexingAndQueryingTestHarness();
   }
 
-  static Object[][] getTestParams() {
-    return new Object[][] {
-      {VectorSimilarity.DOT_PRODUCT, false, 1},
-      {VectorSimilarity.DOT_PRODUCT, true, 1},
-      {VectorSimilarity.DOT_PRODUCT, false, 2},
-      {VectorSimilarity.DOT_PRODUCT, true, 2},
-      {VectorSimilarity.COSINE, false, 1},
-      {VectorSimilarity.COSINE, true, 1},
-      {VectorSimilarity.COSINE, false, 2},
-      {VectorSimilarity.COSINE, true, 2},
-      {VectorSimilarity.EUCLIDEAN, false, 1},
-      {VectorSimilarity.EUCLIDEAN, true, 1},
-      {VectorSimilarity.EUCLIDEAN, false, 2},
-      {VectorSimilarity.EUCLIDEAN, true, 2},
-    };
+  record InternalTestParams(VectorSimilarity similarity, boolean exact, int numPartitions) {}
+
+  record TestCase(
+      VectorSimilarity similarity,
+      boolean exact,
+      int numPartitions,
+      Vector.VectorType vectorType,
+      VectorQuantization vectorQuantization) {}
+
+  static List<InternalTestParams> getInternalTestParams() {
+    return List.of(
+        new InternalTestParams(VectorSimilarity.DOT_PRODUCT, false, 1),
+        new InternalTestParams(VectorSimilarity.DOT_PRODUCT, true, 1),
+        new InternalTestParams(VectorSimilarity.DOT_PRODUCT, false, 2),
+        new InternalTestParams(VectorSimilarity.DOT_PRODUCT, true, 2),
+        new InternalTestParams(VectorSimilarity.COSINE, false, 1),
+        new InternalTestParams(VectorSimilarity.COSINE, true, 1),
+        new InternalTestParams(VectorSimilarity.COSINE, false, 2),
+        new InternalTestParams(VectorSimilarity.COSINE, true, 2),
+        new InternalTestParams(VectorSimilarity.EUCLIDEAN, false, 1),
+        new InternalTestParams(VectorSimilarity.EUCLIDEAN, true, 1),
+        new InternalTestParams(VectorSimilarity.EUCLIDEAN, false, 2),
+        new InternalTestParams(VectorSimilarity.EUCLIDEAN, true, 2));
   }
 
-  void runTest(Vector.VectorType vectorType) throws Exception {
-    runTest(vectorType, VectorQuantization.NONE);
+  @Parameterized.Parameters
+  public static List<TestCase> data() {
+    List<TestCase> params = new ArrayList<>();
+    for (InternalTestParams base : getInternalTestParams()) {
+      VectorSimilarity similarity = base.similarity();
+      boolean exact = base.exact();
+      int numPartitions = base.numPartitions();
+
+      params.add(
+          new TestCase(
+              similarity, exact, numPartitions, Vector.VectorType.FLOAT, VectorQuantization.NONE));
+      params.add(
+          new TestCase(
+              similarity, exact, numPartitions, Vector.VectorType.BYTE, VectorQuantization.NONE));
+      params.add(
+          new TestCase(
+              similarity, exact, numPartitions, Vector.VectorType.BIT, VectorQuantization.NONE));
+      params.add(
+          new TestCase(
+              similarity,
+              exact,
+              numPartitions,
+              Vector.VectorType.FLOAT,
+              VectorQuantization.SCALAR));
+      params.add(
+          new TestCase(
+              similarity,
+              exact,
+              numPartitions,
+              Vector.VectorType.FLOAT,
+              VectorQuantization.BINARY));
+    }
+    return params;
   }
 
-  void runTest(Vector.VectorType vectorType, VectorQuantization vectorQuantization)
-      throws Exception {
+  @Test
+  public void runTest() throws Exception {
+    if (this.vectorType == Vector.VectorType.BIT
+        && this.vectorSimilarity != VectorSimilarity.EUCLIDEAN) {
+      return;
+    }
     try (var harness = this.testHarness) {
       harness.setNumCandidatesAndLimit(NUM_CANDIDATES, LIMIT);
+      if (this.vectorQuantization == VectorQuantization.BINARY) {
+        // Random vectors don't perform well with binary quantization.
+        // Increasing num candidates helps with getting a reasonable recall value.
+        this.testHarness.setNumCandidatesAndLimit(1000, 10);
+        // Also reduce the target recall for binary quantization.
+        this.testHarness.setTargetRecall(0.6);
+      }
       harness.setUp(
           buildVectorIndexDefinition(
-              this.vectorSimilarity, DIMENSIONS, this.numPartitions, vectorQuantization));
-      switch (vectorType) {
+              this.vectorSimilarity, DIMENSIONS, this.numPartitions, this.vectorQuantization));
+      switch (this.vectorType) {
         case FLOAT -> {
           float[][] floatVectors =
               harness.generateNormalizedRandomFloatVectors(NUM_VECTORS, DIMENSIONS);
