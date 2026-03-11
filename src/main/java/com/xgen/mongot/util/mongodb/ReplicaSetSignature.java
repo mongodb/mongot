4db5f367f4d716f8ca7a47b2f6021e9e64456591
idil <111353960+idil-bit@users.noreply.github.com>
2026-03-11 11:54:15 -0400

    CLOUDP-325231 - parse syncSourceMetadata from automation config (#5435)
    GitOrigin-RevId: 219dfb5a538e9e0933a977c64ce90f109875da10

--- src/main/java/com/xgen/mongot/util/mongodb/ReplicaSetSignature.java
+++ src/main/java/com/xgen/mongot/util/mongodb/ReplicaSetSignature.java
@@ -1,13 +1,4 @@
 package com.xgen.mongot.util.mongodb;
 
 // A unique signature per replica set that is not reused upon shard removal and recreation.
-public record ReplicaSetSignature(String rsId, String createDate) {
-  public static ReplicaSetSignature fromSnapshotterBasePrefix(String basePrefix) {
-    var parts = basePrefix.split("/");
-    var rsUniqueId = parts[parts.length - 1];
-    int separatorIdx = rsUniqueId.lastIndexOf('-');
-    var rsId = rsUniqueId.substring(0, separatorIdx);
-    var createDate = rsUniqueId.substring(separatorIdx + 1);
-    return new ReplicaSetSignature(rsId, createDate);
-  }
-}
+public record ReplicaSetSignature(String rsId, String createDate) {}
